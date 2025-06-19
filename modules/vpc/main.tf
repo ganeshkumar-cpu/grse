b3baeb0de7b5b277848c27de42a23c9511a0e914
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-06-19 18:00:26 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -31,4 +31,50 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count             = length(var.private_subnets)
   vpc_id            = aws_vpc.this.id
-  cidr_b_
+  cidr_block        = var.private_subnets[count.index].cidr_block
+  availability_zone = var.private_subnets[count.index].availability_zone
+
+  tags = {
+    Name = "${var.name}-private-${var.private_subnets[count.index].availability_zone}"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-public"
+  }
+}
+
+# Private Route Tables - one per AZ
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+
+  # Add NAT Gateway or other routes here if you have them; else leave blank
+
+  tags = {
+    Name = "${var.name}-private-${var.private_subnets[count.index].availability_zone}"
+  }
+}
+
+# Associate public subnets with public RT
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate each private subnet with its own private RT (matching AZ index)
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
