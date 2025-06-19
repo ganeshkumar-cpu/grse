a873a56bb00a4ec81e4e981a0b4858c3c73e7912
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-06-19 17:34:46 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,25 +1,14 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
-  enable_dns_hostnames = true
   enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = var.name
   }
 }
 
-resource "aws_subnet" "this" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.subnet_cidr
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.name}-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "this" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
 
   tags = {
@@ -27,21 +16,19 @@ resource "aws_internet_gateway" "this" {
   }
 }
 
-resource "aws_route_table" "this" {
-  vpc_id = aws_vpc.this.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index].cidr_block
+  availability_zone       = var.public_subnets[count.index].availability_zone
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.name}-rt"
+    Name = "${var.name}-public-${var.public_subnets[count.index].availability_zone}"
   }
 }
 
-resource "aws_route_table_association" "this" {
-  subnet_id      = aws_subnet.this.id
-  route_table_id = aws_route_table.this.id
-}
-
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_b_
